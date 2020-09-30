6efe0f835db5c10b0c33006baad3888e03d86c77
Your Name <your@email.com>
2020-09-30 10:45:47 +0200

    integrated stripe payment and updated favicon and app title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 
 import { selectCartItems, selectCartTotal  } from '../../redux/cart/cart.selectors';
 
@@ -32,10 +34,16 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
         ))
     }
-    <div className='total'>
-        <span>TOTAL: ${total}</span>
+    <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+        *Please use the following test card for payments*
+        <br/>
+        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+        <br/>
+        Exp. date of this test card increases with the year, so if it is already 2021 then fill in 01/22 etc.
     </div>
-</div>
+        <StripeCheckoutButton price={total} />
+    </div>
 );
 
 const mapStateToProps = createStructuredSelector({
