de8bed1224917b8f02764183902cd64028f9caab
Your Name <your@email.com>
2020-10-05 14:20:42 +0200

    updating with async redux and container patern

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -41,5 +41,5 @@ const mapStateToProps = createStructuredSelector({
 });
 
 
-export default connect(mapStateToProps)(CollectionsOverview)
+export default connect(mapStateToProps)(CollectionsOverview);
 
